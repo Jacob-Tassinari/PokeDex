ebff5287d82116fdfc2abb5351b37b3e1df22759
Jacob Tassinari <jacobtassinari@Interapts-MacBook-Pro-3.local>
2018-11-12 10:21:45 -0600

    Initial Commit

---
+++ Poke-Dex/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Poke-Dex
+//
+//  Created by Jacob Tassinari on 11/12/18.
+//  Copyright © 2018 Jacob Tassinari. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
